42e3aa427442dcc6e8db39d3f17b0b791cf02226
jyotihs721 <jyotisingh1994@gmail.com>
2019-10-30 15:56:40 +0530

    First commit on 30 -10 -2019

---
+++ src/main/java/Test_30_Oct/LearnAutomation/App.java
@@ -0,0 +1,13 @@
+package Test_30_Oct.LearnAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
